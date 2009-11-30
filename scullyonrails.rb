ea94e4f7fdacd1f0e1cc40bfe156144793276025
Brandon Calloway <brandon@scullytown.com>
2009-11-30 13:24:39 -0500

    updated rails version to 2.3.5

--- scullyonrails.rb
+++ scullyonrails.rb
@@ -177,7 +177,7 @@ PROJECT_NAME = "CHANGEME"
 SITE_URL = "localhost:3000"
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '2.3.4' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.3.5' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
